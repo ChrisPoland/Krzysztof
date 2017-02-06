73e9028b7c48f279f3f5f120d91ebd69e6bcf94c
Chris <krzysztof.drapala.99@wp.pl>
2017-02-06 07:25:31 +0000

    aaa

---
+++ 170202/liczby_pierwsze.java
@@ -0,0 +1,25 @@
+public class liczby_pierwsze{
+
+public static void main(String[] args){ 
+Scanner dane=new Scanner(System.in);
+
+int x=dane.next.Int();
+boolean liczba_pierwsza=true;
+
+for(int i=2; i*i<=x;i++)
+
+if(x%i==0)
+
+liczba_pierwsza=false
+
+
+
+if(liczba_pierwsza)
+
+
+
+System.out.println("Tak");
+else
+System.out.println("Nie");
+}
+}
