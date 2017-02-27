89cd72bc13f49013ff915e5c634bd986d1ac2a56
Alicja <filipczuka@go2.pl>
2017-02-27 08:23:23 +0000

    aa

--- Ferie_zimowe/pary_liczb.java
+++ Ferie_zimowe/pary_liczb.java
@@ -1,36 +1,64 @@
 import java.io.File;
-import java.FileNotFoundException;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.PrintWriter;
+
 public class pary_liczb{
-    int NWD(int a,int b){
-        while(a!=b){
-            if(a>b) a=a-b;
-            else b=b-a;
-            
-        }
-        return a;
-    }
-    int sumacyfry (int n){
-        int s=0;
-        while(n>0){
-            s=s+n%10;
-            n=n/10;
-        }
-        return s;
-    }
     
-    public static main void (String[] args)throws FileNotFoundException{
-        pary_liczb.funkcje=new pary_liczb();
-        File file= new File ("PARY_LICZB.txt");
-        Scanner in=new Scanner(file);
-        int liczba1,liczba2;
-        while(in.hasNextLine()){
-            liczba1=in.nextInt();
-            liczba2=in.nextInt();
-            System.out.println("l1"+liczba1+"(2"+liczba2);
+    //                           FUNKCJE
+    
+   
+    
+    int NWD(int a, int b){
+        
+        while(a!=b){
+            if(a>b) a=a-b; else b=b-a;
         }
         
-       
+         return a;
+        
     }
-}
 
+    int sumacyfr(int a){
+        int s=0;
+        while(a>0)
+        {s=s+a%10;
+        a=a/10;}
+        return s;
+    }
+
+
+//                            PROGRAM GŁÓWNY
+    public static void main(String[] args) throws FileNotFoundException{
+
+   
+   
+
+       //przygotowanie do odczytu z pliku liczbyinaczej.txt
+        File file = new File("PARY_LICZB.TXT");
+        Scanner in=new Scanner(file);
+        
+       //przygotowanie do zapisu do pliku zadanie5.txt
+        PrintWriter zapis=new PrintWriter("zadanie5.txt");
+        
+        //Tworze obiekt naszej klasy o nazwie funkcje, aby móc później wywoływać np. funkcje.BinToDec()
+        pary_liczb funkcje=new pary_liczb();
+       
+       
+       int l1,l2;
+       
+        //pobieramy kolejne linijki pliku, zapisujemy je w zmiennej tekst, a ich długosć w zm. size
+         while(in.hasNextLine()){
+             l1=in.nextInt();
+            l2 =in.nextInt();
+           
+             System.out.println(l1+"aaa"+l2);
+        }
+
+  
+        
+   
+     
+    }
+
+}
