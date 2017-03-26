35fd5a545cabab7ecafce98ddbce9c4fac7ba3e4
Chris <krzysztof.drapala.99@wp.pl>
2017-03-26 19:16:35 +0000

    .

---
+++ 170320/zadanie1.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.PrintWriter;
+public class zadanie1{
+ 
+ int s;
+ int l=1;
+int i;
+ s=liczba;
+ while(l!=1)
+ {
+     s==s/2;
+     s++;
+
+ 
+}
+
+String DecToBin (int s)
+        String tekst="";
+        while (s>0){ 
+            tekst=tekst+((char)(s%2+48));
+            s=s/2;
+    }
+      String reverse= new StringBuffer(tekst).reverse().toString();
+            return reverse;
+}
+    System.out.println("W systemie binarnym:");
+    for(int i=0;i<l;i++)
+    System.out.println(tab[i]);
+    System.out.println("liczba pol, ktora wyswietla 1:");
+    for(int i=0; i<l; i++){
+        if(tab[l]==1)
+        l++;
+        System.out.println("Liczba jedynek:");
+        
+    
+}
+       
+   }
---
+++ 170320/zadanie2.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.PrintWriter;
+public class zadanie2{
+    
+}
+ String BinToDec (int n){
+        String tekst="";
+        while (n>0){ 
+            tekst=tekst+((char)(n%10+48));
+            n=n/10;
+        }
+    
+        String reverse= new StringBuffer(tekst).reverse().toString();
+            return reverse;
+    }
+    public static void main(String[]args)throws FileNotFoundException{
+        String s,zl,gr;
+        float w;
+        int tmp;
+        System.out.println("Podaj cene w systemie binarnym z dokladnoscia do dwoch cyfr po przecinku")
+        for(tmp=0;tmp<s.size();i++)
+        for(int i=0;i<zl.size();i++)
+        for(int i=0;i<gr.size();i++)
+        System.out.println("Cena w systemie dzisietnym wynosi:")
+        
